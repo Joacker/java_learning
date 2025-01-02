0714929e6b172ec1a5dc29af7f422301dc79da44
Joacker <jfernandezvizcarra@gmail.com>
2025-01-02 14:22:51 -0300

    adding some features to controllers like user for envirnment testing

--- src/main/java/com/joaquin/springboot/webapp/springboot_web/controllers/UserController.java
+++ src/main/java/com/joaquin/springboot/webapp/springboot_web/controllers/UserController.java
@@ -7,6 +7,7 @@ import java.util.Arrays;
 import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.stereotype.Controller;
 import com.joaquin.springboot.webapp.springboot_web.models.User;
 
@@ -30,14 +31,25 @@ public class UserController {
 
     @GetMapping("/list")
     public String list(ModelMap model) {
-       List<User> users = Arrays.asList(
+    //    List<User> users = Arrays.asList(
+    //     new User("pepa", "gonzalez"), 
+    //     new User("lola", "lopez", "lola@correo.com"), 
+    //     new User("pepe", "perez", "pepe@correo.com"),
+    //     new User("joaquin", "doe", "joaquin@corre.com")); 
+
+    //    model.addAttribute("users", users);
+       model.addAttribute("title", "Listado de usuarios");
+        return "list";
+    }
+
+    @ModelAttribute("users")
+    public List<User> usersModel (){
+        List<User> users = Arrays.asList(
         new User("pepa", "gonzalez"), 
         new User("lola", "lopez", "lola@correo.com"), 
         new User("pepe", "perez", "pepe@correo.com"),
         new User("joaquin", "doe", "joaquin@corre.com")); 
 
-       model.addAttribute("users", users);
-       model.addAttribute("title", "Listado de usuarios");
-        return "list";
+        return users;
     }
 }
