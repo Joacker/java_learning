41c18d7d688b495f6519346b2595774bfda5ff4d
Joacker <jfernandezvizcarra@gmail.com>
2025-01-02 14:52:36 -0300

    adding params into routing

---
+++ src/main/java/com/joaquin/springboot/webapp/springboot_web/controllers/RequestParamsController.java
@@ -0,0 +1,20 @@
+package com.joaquin.springboot.webapp.springboot_web.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.joaquin.springboot.webapp.springboot_web.models.dto.ParamDto;
+
+@RestController
+@RequestMapping("/api/params")
+public class RequestParamsController {
+
+    @GetMapping("/foo")
+    public ParamDto foo(@RequestParam String message) {
+        ParamDto paramDto = new ParamDto();
+        paramDto.setMessage(message);
+        return paramDto;
+    }
+}
