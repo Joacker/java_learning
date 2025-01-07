924b3da066fdb4bd3780cbcb3cd78b16c82513e3
Joacker <jfernandezvizcarra@gmail.com>
2025-01-07 12:10:38 -0300

    pathbariables added

--- src/main/java/com/joaquin/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/joaquin/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -3,16 +3,20 @@ package com.joaquin.springboot.webapp.springboot_web.controllers;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import com.joaquin.springboot.webapp.springboot_web.models.dto.ParamDto;
 
 
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
 
-    @GetMapping("/baz")
-    public String getMethodName(@RequestParam String param) {
-        return new String();
+    @GetMapping("/baz/{message}")
+    public ParamDto baz(@PathVariable String message) {
+        ParamDto param = new ParamDto();
+        param.setMessage(message);
+        return param;
     }
     
 }
