e179e900c0415528169a8f3aedc18604b817b703
Joacker <jfernandezvizcarra@gmail.com>
2025-01-07 17:06:28 -0300

    pathvariables multi added

--- src/main/java/com/joaquin/springboot/webapp/springboot_web/controllers/PathVariableController.java
+++ src/main/java/com/joaquin/springboot/webapp/springboot_web/controllers/PathVariableController.java
@@ -7,16 +7,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import com.joaquin.springboot.webapp.springboot_web.models.dto.ParamDto;
 
-
+import java.util.Map;
+import java.util.HashMap;
 @RestController
 @RequestMapping("/api/var")
 public class PathVariableController {
 
     @GetMapping("/baz/{message}")
-    public ParamDto baz(@PathVariable(name = "mensaje") String message) {
+    public ParamDto baz(@PathVariable String message) {
         ParamDto param = new ParamDto();
         param.setMessage(message);
         return param;
+    }
+    
+    @GetMapping("/mix/{product}/{id}")
+    public Map<String, Object> mixPathVar(@PathVariable String product, @PathVariable Long id) {
+        
+        Map<String, Object> json = new HashMap<>();
+        json.put("product", product);
+        json.put("id", id);
+        
+        return json;
     }
     
 }
