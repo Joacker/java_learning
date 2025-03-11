50e365af0039fbf1cc85b3df9ed9a74763b7b801
Joacker <jfernandezvizcarra@gmail.com>
2025-03-10 23:48:31 -0300

    adding new features

---
+++ apirest-tutorial/demo/src/main/java/com/example/demo/demo/entity/Articulo.java
@@ -0,0 +1,36 @@
+package com.example.demo.demo.entity;
+
+import java.util.Date;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+
+
+@Entity
+@Table(name = "articulos")
+public class Articulo {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(unique = true, nullable = false, length = 100)
+    private String nombre;
+    @Column(nullable = false)
+    private Double precio;
+    @Column(name = "created_at", nullable = false, updatable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    private Date createAt;
+    @Column(name = "updated_at", nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    private Date updateAt;
+}
