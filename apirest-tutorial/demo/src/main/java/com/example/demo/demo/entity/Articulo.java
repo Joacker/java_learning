f9a973623f793987464639bda0d1747ae5abe8bd
Joacker <jfernandezvizcarra@gmail.com>
2025-03-11 00:30:42 -0300

    adding new features

--- apirest-tutorial/demo/src/main/java/com/example/demo/demo/entity/Articulo.java
+++ apirest-tutorial/demo/src/main/java/com/example/demo/demo/entity/Articulo.java
@@ -4,18 +4,29 @@ import java.util.Date;
 
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
 import jakarta.persistence.TemporalType;
 
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.Getter;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
 @Entity
+@EntityListeners(AuditingEntityListener.class)
 @Table(name = "articulos")
 public class Articulo {
     @Id
