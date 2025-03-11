c3c26c42b458262fd2338b2a01824f9e1c5349f9
Joacker <jfernandezvizcarra@gmail.com>
2025-03-11 10:03:46 -0300

    adding new features

--- apirest-tutorial/demo/src/main/java/com/example/demo/demo/controller/ArticuloController.java
+++ apirest-tutorial/demo/src/main/java/com/example/demo/demo/controller/ArticuloController.java
@@ -1,24 +1,27 @@
 package com.example.demo.demo.controller;
 
 import org.springframework.data.domain.Pageable;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
 
 import com.example.demo.demo.service.ArticuloService;
 import com.example.demo.demo.entity.Articulo;
 
 import java.util.List;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.PathVariable;
-
 import org.springframework.web.bind.annotation.RequestBody;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -73,4 +76,9 @@ public class ArticuloController {
         }
     }
 
+    @DeleteMapping(value="/{id}")
+    public ResponseEntity<Void> delete(@PathVariable("id") int id) {
+        service.delete(id);
+        return ResponseEntity.ok(null);
+    }
 }
