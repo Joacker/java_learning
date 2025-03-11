faa09484d489ada839ce4c3dbbe64463c3d9bed8
Joacker <jfernandezvizcarra@gmail.com>
2025-03-11 00:41:09 -0300

    adding new features

--- apirest-tutorial/demo/src/main/java/com/example/demo/demo/repository/ArticuloRepository.java
+++ apirest-tutorial/demo/src/main/java/com/example/demo/demo/repository/ArticuloRepository.java
@@ -1,7 +1,13 @@
 package com.example.demo.demo.repository;
 import com.example.demo.demo.entity.Articulo;
-import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
+import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public interface ArticuloRepository extends  JpaRepository<Articulo, Integer> {
-    
+    List<Articulo> findByNombreContaining(String nombre, Pageable page);
 }
