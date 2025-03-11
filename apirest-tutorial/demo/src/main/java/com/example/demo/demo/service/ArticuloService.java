5062cee07f5213a74e55aab1a9d251e65846b6a4
Joacker <jfernandezvizcarra@gmail.com>
2025-03-11 00:43:34 -0300

    adding new features

---
+++ apirest-tutorial/demo/src/main/java/com/example/demo/demo/service/ArticuloService.java
@@ -0,0 +1,8 @@
+package com.example.demo.demo.service;
+
+import com.example.demo.demo.entity.Articulo;
+import java.util.List;
+
+public interface ArticuloService {
+    public List<Articulo> findAll();
+}
