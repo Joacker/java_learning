908350300e2eb75ea797760fcb493a7888f98241
Joacker <jfernandezvizcarra@gmail.com>
2025-03-11 09:35:46 -0300

    adding new features

---
+++ apirest-tutorial/demo/src/main/java/com/example/demo/demo/controller/ArticuloController.java
@@ -0,0 +1,61 @@
+package com.example.demo.demo.controller;
+
+import org.springframework.data.domain.Pageable;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.demo.service.ArticuloService;
+import com.example.demo.demo.entity.Articulo;
+
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import org.springframework.web.bind.annotation.RequestBody;
+
+import org.springframework.http.ResponseEntity;
+
+@RestController
+@RequestMapping("/version1/articulos")
+public class ArticuloController {
+    @Autowired
+    private ArticuloService service;
+
+    @GetMapping("path")
+    public ResponseEntity<List<Articulo>> findAll(
+            @RequestParam(value = "nombre", required = false, defaultValue = "") String nombre,
+            @RequestParam(value = "offset", required = false, defaultValue = "0") int pageNumber,
+            @RequestParam(value = "limit", required = false, defaultValue = "5") int pageSize
+            ) {
+        Pageable page = PageRequest.of(pageNumber, pageSize);
+        List<Articulo> articulos;
+        if (nombre==null || nombre.isEmpty()) {
+            articulos = service.findAll(page);
+        } else {
+            articulos = service.findByNombre(nombre, page);
+        }
+
+        if (articulos == null) {
+            return ResponseEntity.notFound().build();
+        } 
+        return ResponseEntity.ok(articulos);
+    }
+
+    public ResponseEntity<Articulo> findById(@PathVariable("id") int id) {
+        Articulo articulo = service.findById(id);
+        if (articulo == null) {
+            return ResponseEntity.notFound().build();
+        } else {
+            return ResponseEntity.ok(articulo);
+        }
+    }
+
+    public ResponseEntity<Articulo> create(@RequestBody Articulo articulo) {
+        Articulo registro = service.save(articulo);
+        return ResponseEntity.ok(registro);
+    }
+
+}
--- apirest-tutorial/demo/src/main/java/com/example/demo/demo/repository/ArticuloRepository.java
+++ apirest-tutorial/demo/src/main/java/com/example/demo/demo/repository/ArticuloRepository.java
@@ -3,7 +3,7 @@ import com.example.demo.demo.entity.Articulo;
 
 import java.util.List;
 
-import org.springframework.boot.autoconfigure.data.web.SpringDataWebProperties.Pageable;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
